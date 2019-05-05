bf13690d5b2729e1b8fb90521354db08b6299dca
Syahrul Akbar Rohmani <sahruldotid@gmail.com>
2019-05-05 16:35:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ export PATH=$PATH:`pwd`
 wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O /usr/local/bin/apktool
 wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.0.jar -O /usr/local/bin/apktool.jar
 chmod +x /usr/local/bin/apktool*
+cp aplod /usr/local/bin/
+chmod +x /usr/local/bin/aplod
