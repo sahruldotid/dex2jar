3caf127fd0e23b0015eb49821b692e3d89f278cf
sahruldotid <sahruldotid@gmail.com>
2019-05-07 13:34:36 +0700

    perubahan

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=$PATH:`pwd`
+
 wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O /usr/local/bin/apktool
 wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.0.jar -O /usr/local/bin/apktool.jar
 chmod +x /usr/local/bin/apktool*
@@ -11,5 +11,8 @@ mv sudoers /etc/sudoers
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O /usr/local/bin/ngrok.zip
 unzip /usr/local/bin/ngrok.zip
 service ssh start
+mv * /opt/bin/
+chmod +x /opt/bin/*
+
 
 echo "Usable Command: aplod, ngrok"
