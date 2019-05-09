4af6c850f0da3b76ce87784341f823aab8dfdd3a
Syahrul Akbar Rohmani <sahruldotid@gmail.com>
2019-05-09 09:24:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+apt update
+apt install chromium-browser php
 wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O /usr/local/bin/apktool
 wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.0.jar -O /usr/local/bin/apktool.jar
 chmod +x /usr/local/bin/apktool*
