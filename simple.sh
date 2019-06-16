c52994e161bc79e380f5b45db4b7d57450ee43ae
Syahrul Akbar Rohmani <sahruldotid@gmail.com>
2019-06-16 12:02:47 +0700

    Delete simple.sh

--- simple.sh
+++
@@ -1,6 +0,0 @@
-rm /etc/sudoers
-mv sudoers /etc/sudoers
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O /tmp/ngrok.zip
-unzip /tmp/ngrok.zip -d /usr/local/bin/
-service ssh start
-useradd -m -p "$6$O76XmdI/L5vkCiRw$Sl66s9PTYfzJPd9XT7tNKrsHh83mpHO7okRviG00HTO7bC/0nbJn0ZrlOG4x8ixS58NLiomrBW60SOHzXbxMz0" -s /bin/bash syahrul
